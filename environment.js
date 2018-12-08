35d1dabb32e27abaf2da7a77f3b6e4aefbeb5367
Bharadwaj <bparthas@buffalo.edu>
2018-12-08 11:17:47 -0500

    environment js

--- environment.js
+++ environment.js
@@ -1,7 +1,7 @@
 var environmentVariables = {
-    'APP_SERVER_URL':'http://192.168.1.178:8010',
-    'APP_DB_URL':'192.168.1.178',
-    'APP_DB_PORT':'8011',
+    'APP_SERVER_URL':'',
+    'APP_DB_URL':'',
+    'APP_DB_PORT':'',
     'APP_DB_USERNAME':'root',
     'APP_DB_PASSWORD':'root'
 };
